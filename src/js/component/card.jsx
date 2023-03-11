5a1f39f31b5c11556c00ed2e8d38db08451deb2b
Doctorine06 <fergtheartist@gmail.com>
2023-03-11 01:58:15 +0000

    new commit

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card =() => {
     return (
-        <div className="card" h-40rem>
+        <div className="card" h-40>
   <img src="https://picsum.photos/70/100.jpg" className="card-img-top" alt="https://picsum.photos/70/100" />
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
